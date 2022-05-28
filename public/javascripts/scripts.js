ead24cf7cc832a24e80aa6befd9495b14b97a02e
Veronika Petersen <veronika.petersen@deptagency.com>
2022-05-28 22:40:09 +0200

    fix

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -11,12 +11,10 @@ document.querySelector("#show_users").addEventListener('click', ()=> {
             console.log(data);
 
             let usersContainer = document.querySelector("#users")
-            let div = document.createElement("div");
-            div.innerHTML = 'Name: ' + data[0].userFirstName + '\n' + data[0].userLastName;
-            usersContainer.appendChild(div);
-
+            usersContainer.innerHTML = 'Name: ' + data[0].userFirstName + '\n' + data[0].userLastName;
+            // usersContainer.appendChild(div);
         })
-        .catch((error) => {
+        .catch((err) => {
             throw err;
         })
 
