b86986484a9f7e09ec11c02b31f6985a4be5fef3
Veronika Petersen <veronika.petersen@deptagency.com>
2022-06-12 03:48:23 +0200

    user - edit my info form

--- routes/index.js
+++ routes/index.js
@@ -28,7 +28,7 @@ router.get('/signup', function (req, res, next) {
 router.get('/home_instructor', function (req, res, next) {
   let session = req.session;
   if (session.loggedin) {
-    res.render('home_instructor', { title: 'YogaLand', whoami: ': Instructors', data:data });
+    res.render('home_instructor', { title: 'YogaLand', whoami: ': Instructors', data: data });
   } else {
     res.redirect('/');
   }
@@ -58,22 +58,34 @@ router.get('/logout', function (req, res, next) {
 
 
 router.get('/users/:id/edit', function (req, res, next) {
+  // let session = req.session;
 
-  let session = req.session;
-  if (session.loggedin) {
-    // res.render('', { title: 'YogaLand' });
+  // if (session.loggedin) {
+  createClient();
+  client.connect();
+  client.query('SELECT * FROM users WHERE users.user_id = $1', [req.params.id], (err, result) => {
+    if (err) throw err;
+    let data = result.rows[0];
+    console.log(data);
+    client.end();
+    res.render('edit', { title: 'YogaLand', data: data });
+  })
 
-    createClient();
-    client.connect();
-    client.query('SELECT * FROM users WHERE user.user_id = $1', [req.params.id], (err, result) => {
-      console.log(req.params.id)
-    })
-
-  } else {
-    res.redirect('/');
-  }
+  // } else {
+  //   res.redirect('/');
+  // }
 
 })
+
+
+
+
+
+
+
+
+
+
 
 
 router.get('/class/:id', function (req, res, next) {
@@ -95,7 +107,6 @@ router.get('/class/:id', function (req, res, next) {
 
 
 router.get('/instructor/:id', function (req, res, next) {
-
   let session = req.session;
   if (session.loggedin) {
     createClient();
@@ -104,21 +115,15 @@ router.get('/instructor/:id', function (req, res, next) {
     client.query('SELECT * FROM public.instructors JOIN public.reviews on instructors.instructor_id = reviews.instructor_id WHERE instructors.instructor_id = $1', [req.params.id], (err, result) => {
       if (err) throw err;
       let data = result.rows[0];
-
       //reviews for instructor
       client.query('SELECT * FROM public.reviews JOIN public.users on reviews.user_id = users.user_id WHERE reviews.instructor_id = $1', [req.params.id], (err, result) => {
         if (err) throw err;
         let datareviews = result.rows;
-
         //upcoming classes for instructor
         client.query('SELECT *, (SELECT COUNT(*) AS reservations FROM public.bookings WHERE bookings.class_id = classes.class_id) FROM public.classes JOIN public.locations on classes.location_id = locations.location_id WHERE classes.instructor_id = $1', [req.params.id], (err, result) => {
-
           if (err) throw err;
           let upcomingClasses = result.rows;
-          console.log(upcomingClasses)
-
           client.end();
-
           res.render('instructor', {
             title: 'YogaLand',
             first_name: data.instructor_first_name,
@@ -128,11 +133,8 @@ router.get('/instructor/:id', function (req, res, next) {
             upcomingClasses: upcomingClasses
           });
         });
-
       });
-
     })
-
   } else {
     res.redirect('/');
   }
@@ -227,19 +229,14 @@ router.post('/login', function (req, res, next) {
         // initialize session
         let session = req.session;
         session.loggedin = true;
-        // req.session.loggedin = true;
         data = result.rows;
         res.redirect(redirectUrl);
-        // res.render('index', { title: 'YogaLand', data: data});
-        // res.send(data);
         client.end();
       }
       else {
         console.log("user not found");
         client.end();
         res.redirect(redirectUrl);
-        // store the error message 
-        // pass it to the frontend with the request response method inside a function
       }
     });
   }
@@ -279,5 +276,31 @@ router.post('/signup', function (req, res, next) {
   }
 });
 
+
+
+router.post('/edit_user_info', function (req, res, next) {
+  let signup_useremail = req.body.useremail;
+  let signup_first_name = req.body.first_name;
+  let signup_last_name = req.body.last_name;
+  let signup_street = req.body.street;
+  let signup_building = req.body.building;
+  let signup_zipcode = req.body.zipcode;
+  let id;
+  createClient();
+  client.connect();
+  sql = ' SELECT * FROM public.users WHERE users.user_email = $1';
+  client.query(sql, [signup_useremail], function (err, result) {
+    id = result.rows[0].user_id;
+
+    sql = "UPDATE public.users SET user_email = $1, user_first_name = $2, user_last_name = $3, user_street = $4, user_building = $5, user_zipcode = $6 WHERE user_id = $7";
+    client.query(sql, [signup_useremail, signup_first_name, signup_last_name, signup_street, signup_building, signup_zipcode, id], function (err, result) {
+      if (err) throw err;
+      client.end();
+      console.log("changes saved");
+    })
+
+  })
+
+})
 
 module.exports = router;
